20c7d7a95815acbd82c330ac8c592b3745bdac67
nedtonks <104468000@student.swin.edu.au>
2024-05-22 16:17:36 +1000

    settings.php
    settings.php

--- settings.php
+++ settings.php
@@ -1,1 +1,6 @@
-
+<?php
+	$host = "feenix-mariadb.swin.edu.au";
+	$user = "s104468000"; // your user name
+	$pwd = "090703"; // your password (date of birth ddmmyy unless changed)
+	$sql_db = "s104468000_db"; // your database
+?>
